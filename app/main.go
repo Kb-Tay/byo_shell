5337e4fd5a4676ceaee2ab06d3a3223ec89309f4
Aaron <taykaiboon12@gmail.com>
2026-08-16 13:38:08 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,6 +12,11 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+var commandMap = map[string]string{
+	"echo": "echo is a shell builtin",
+	"exit": "exit is a shell builtin",
+}
+
 func main() {
 	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
 
@@ -33,6 +38,14 @@ func main() {
 			cmd := args[0]
 
 			switch cmd {
+			case "type":
+				info, ok := commandMap[cmd]
+				if !ok {
+					fmt.Print(strings.Join(args[1:], "") + ": command not found")	
+					break	
+				}
+
+				fmt.Println(info)
 			case "exit":
 				return
 			case "echo":
@@ -44,3 +57,4 @@ func main() {
 		}
 	}
 }
+
